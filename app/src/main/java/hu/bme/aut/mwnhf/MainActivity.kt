9ca6ad5fa34cf8c5d75d2a6651581dcd7f7bece1
Beke Ambrus <Ambrus.Beke@kh.hu>
2021-12-15 00:06:18 +0100

    Stoppwatch implemented with restarts

--- app/src/main/java/hu/bme/aut/mwnhf/MainActivity.kt
+++ app/src/main/java/hu/bme/aut/mwnhf/MainActivity.kt
@@ -2,10 +2,29 @@ package hu.bme.aut.mwnhf
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
+import android.os.SystemClock.elapsedRealtime
+import android.widget.Chronometer
+import android.widget.TextView
+import hu.bme.aut.mwnhf.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var isRunning: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.start.setOnClickListener {
+            binding.time.base = SystemClock.elapsedRealtime()
+            binding.time.start()
+        }
+        binding.stop.setOnClickListener {
+            binding.time.stop()
+        }
     }
 }
