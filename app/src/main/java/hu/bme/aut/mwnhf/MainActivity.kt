7080fbf663aba962a34de8a490edd65af6a1d1e1
Beke Ambrus <Ambrus.Beke@kh.hu>
2021-12-15 14:16:59 +0100

    Coord tracking works

--- app/src/main/java/hu/bme/aut/mwnhf/MainActivity.kt
+++ app/src/main/java/hu/bme/aut/mwnhf/MainActivity.kt
@@ -1,12 +1,22 @@
 package hu.bme.aut.mwnhf
 
-
+import android.Manifest
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.SystemClock.elapsedRealtime
 import android.util.Log
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
+
+
 import hu.bme.aut.mwnhf.data.Trip
 import hu.bme.aut.mwnhf.data.TripDb
 import hu.bme.aut.mwnhf.databinding.ActivityMainBinding
@@ -17,11 +27,23 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var database: TripDb
+    private lateinit var locationManager: LocationManager
+    private final var MIN_TIME: Long  = 1
+    private final var MIN_DIST: Float = 1.0f
+    lateinit var mLastLocation: Location
+    private lateinit var thetext: TextView
+
+    private val locationListener: LocationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            thetext.text = ("" + location.longitude + ":" + location.latitude)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
+        thetext = binding.cords
         val view = binding.root
 
         setContentView(view)
@@ -41,6 +63,25 @@ class MainActivity : AppCompatActivity() {
             binding.start.isEnabled = false
             binding.stop.isEnabled = true
             actualTrip.startTime = Date().time
+            startTripTracker()
+        }
+    }
+
+    private fun startTripTracker() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION), PackageManager.PERMISSION_GRANTED)
+
+        } else {
+            locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0L, 0f, locationListener)
         }
     }
 
@@ -56,7 +97,12 @@ class MainActivity : AppCompatActivity() {
             binding.time.text = getString(R.string.zeroz)
             binding.start.isEnabled = true
             binding.stop.isEnabled = false
+            stopTripTracker()
         }
+    }
+
+    private fun stopTripTracker(){
+        locationManager.removeUpdates(locationListener)
     }
 
     private fun addHistoryOnClick() {
