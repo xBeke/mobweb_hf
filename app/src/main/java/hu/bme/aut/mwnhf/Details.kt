0a66a424c89b8341a4da020e34a1a65dcaf37977
Beke Ambrus <Ambrus.Beke@kh.hu>
2021-12-15 05:48:05 +0100

    Add details

---
+++ app/src/main/java/hu/bme/aut/mwnhf/Details.kt
@@ -0,0 +1,23 @@
+package hu.bme.aut.mwnhf
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import hu.bme.aut.mwnhf.databinding.ActivityDetailsBinding
+
+class Details : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityDetailsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
